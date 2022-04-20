c54954bee72bd879d1e6e6231d5f4b9dcb056f49
Sonjieun <sonjieun2661@gmail.com>
2022-04-20 10:32:37 +0900

    0420

--- 0418/daynight.js
+++ 0418/daynight.js
@@ -1,27 +1,23 @@
-//js를 분리하여 재사용 가능
-let as = document.querySelectorAll("a");
-
-function night() {
-  document.querySelector("body").style.backgroundColor = "black";
-  document.querySelector("body").style.color = "white";
-  //jquery:$(‘body’).css(‘backgroundColor’,”black”).css(‘color’,’white’)
-  for (let i = 0; i < as.length; i = i + 1) {
-    as[i].style.color = "white";
-  }
-  //jquery:$(‘a’).css(‘color’,’white’)
-  // 이 버튼의 value값을 day로 변경한다.
+function night(){
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.color = 'white';
+    let as = document.querySelectorAll('a');
+    for(let i=0; i<as.length; i=i+1){
+        as[i].style.color='white';
+    }
 }
-//+jquery toggle: <div id="target">toogle</div> <input type="button" value="hide/show" onclick="$('#target').toggle();">
-function day() {
-  document.querySelector("body").style.backgroundColor = "white";
-  document.querySelector("body").style.color = "black";
-  // 이 버튼의 value값을 night로 변경한다.
-  for (let i = 0; i < as.length; i = i + 1) {
-    as[i].style.color = "black";
-  }
+export function day(){
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'black';
+    let as = document.querySelectorAll('a');
+    for(let i=0; i<as.length; i=i+1){
+        as[i].style.color='black';
+    } 
 }
-//외부에서 사용할 function
-export function dayNight(mode) {
-  if (mode === "night") night();
-  else day();
+export function dayNight(mode){
+    if(mode === 'night'){
+        night();
+    } else {
+        day();
+    }
 }
